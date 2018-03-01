531f33149c368fbbf065ca9954ac2d7405ca1389
Chris Nuernberger <chris@thinktopic.com>
2018-03-01 11:33:27 -0700

    Release 0.3.1

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject techascent/tech.javacpp-datatype "0.3.1-SNAPSHOT"
+(defproject techascent/tech.javacpp-datatype "0.3.1"
   :description "Bindings between javacpp and tech.datatype"
   :url "http://github.com/tech-ascent/tech.javacpp-datatype"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [techascent/tech.datatype "0.4.0"]
+                 [techascent/tech.datatype "0.4.1"]
                  [org.bytedeco/javacpp "1.4"]
                  [thinktopic/think.resource "1.2.1"]]
 
