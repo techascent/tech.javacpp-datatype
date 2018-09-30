0843d3e219fddb5908950aeb06a735672e270ecb
Chris Nuernberger <chris@thinktopic.com>
2018-09-30 14:03:01 -0600

    Release 0.3.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.javacpp-datatype "0.3.3-SNAPSHOT"
+(defproject techascent/tech.javacpp-datatype "0.3.3"
   :description "Bindings between javacpp and tech.datatype"
   :url "http://github.com/tech-ascent/tech.javacpp-datatype"
   :license {:name "Eclipse Public License"
--- test/tech/javacpp_datatype_test.clj
+++ test/tech/javacpp_datatype_test.clj
@@ -2,22 +2,23 @@
   (:require [clojure.test :refer :all]
             [tech.javacpp-datatype :as jcpp-dtype]
             [tech.datatype.base :as base]
-            [tech.datatype.marshal :as marshal])
+            [tech.datatype.marshal :as marshal]
+            [think.resource.core :as resource])
   ;;required to load some of the javacpp help functions; they are small functions compiled into each
   ;;bound library.
-  (:import [org.bytedeco.javacpp opencv_core])
-)
+  (:import [org.bytedeco.javacpp opencv_core]))
 
 ;;Force loading of the class to make unit tests work
 (println opencv_core/ACCESS_FAST)
 
 
 (deftest ensure-fast-copy-paths
-  (with-bindings {#'base/*error-on-slow-path* true}
-    (let [int-data (int-array [1 2 3 4 5 6])
-          result (jcpp-dtype/make-pointer-of-type :float32 6)
-          short-data (short-array 6)]
-      (base/copy! int-data 0 result 0 6)
-      (base/copy! result 0 short-data 0 6)
-      (is (= [1 2 3 4 5 6]
-             (vec short-data))))))
+  (resource/with-resource-context
+    (with-bindings {#'base/*error-on-slow-path* true}
+      (let [int-data (int-array [1 2 3 4 5 6])
+            result (jcpp-dtype/make-pointer-of-type :float32 6)
+            short-data (short-array 6)]
+        (base/copy! int-data 0 result 0 6)
+        (base/copy! result 0 short-data 0 6)
+        (is (= [1 2 3 4 5 6]
+               (vec short-data)))))))
