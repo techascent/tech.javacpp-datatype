7d8e0d0f6008eafa6e79679aa34d2599036ca5a2
Chris Nuernberger <chris@techascent.com>
2018-10-13 12:02:51 -0600

    Latest datatype library.

--- project.clj
+++ project.clj
@@ -4,12 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [techascent/tech.datatype "0.5.10"]
+                 [techascent/tech.datatype "0.5.11"]
                  [org.bytedeco/javacpp "1.4"]
                  [thinktopic/think.resource "1.2.1"]]
 
   :profiles {:dev
-             ;; there are a set of small functions that aren't compiled into the javacpp library but into each
-             ;; presets library.  So in order to test or do development we have to load one of the presets libraries;
-             ;; any one that uses javacpp will do
+             ;; there are a set of small functions that aren't compiled into the javacpp
+             ;; library but into each presets library.  So in order to test or do
+             ;; development we have to load one of the presets libraries; any one that
+             ;; uses javacpp will do
              {:dependencies [[org.bytedeco.javacpp-presets/opencv-platform "3.4.0-1.4"]]}})
