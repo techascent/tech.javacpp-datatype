b25e13392eed5c0ff21a02a25a2132ba5c9ab381
Chris Nuernberger <chris@thinktopic.com>
2018-10-22 09:05:36 -0600

    1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.javacpp-datatype "1.0-SNAPSHOT"
+(defproject techascent/tech.javacpp-datatype "1.0"
   :description "Bindings between javacpp and tech.datatype"
   :url "http://github.com/tech-ascent/tech.javacpp-datatype"
   :license {:name "Eclipse Public License"
