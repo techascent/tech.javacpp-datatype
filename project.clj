3f036477d6e90df41bee73c355fe4c2be9fd0ae5
Chris Nuernberger <chris@thinktopic.com>
2018-12-30 15:00:39 -0700

    added deps.edn

--- project.clj
+++ project.clj
@@ -3,13 +3,7 @@
   :url "http://github.com/tech-ascent/tech.javacpp-datatype"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.bytedeco/javacpp "1.4"]
-                 [techascent/tech.datatype "3.0"]]
-
-  :profiles {:dev
-             ;; there are a set of small functions that aren't compiled into the javacpp
-             ;; library but into each presets library.  So in order to test or do
-             ;; development we have to load one of the presets libraries; any one that
-             ;; uses javacpp will do
-             {:dependencies [[org.bytedeco.javacpp-presets/opencv-platform "3.4.0-1.4"]]}})
+  :plugins [[lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+  :profiles {:dev {:lein-tools-deps/config {:resolve-aliases [:test]}}})
